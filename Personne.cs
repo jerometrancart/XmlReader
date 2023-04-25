d62a8485e931d5d364817d4bb4757c70325c2cef
jerometrancart <jerome.trancart@gmail.com>
2023-04-25 12:38:14 +0200

    Xml Reader done

---
+++ Personne.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ExerciceXmlReader
+{
+    public class Personne
+    {
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public DateTime DateDeNaissance { get; set; }
+        public int Taille { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Xml;
+using ExerciceXmlReader;
+
+var reader = XmlReader.Create("fichier.xml");
+var personne = new Personne();
+
+///SE PLACE SUR LA BALISE INITIALE
+reader.MoveToContent();
+///AVANCE SUR L ELEMENT PERSONNE
+reader.ReadStartElement("Personne");
+///AVANCE SUR LA BALISE D'OUVERTURE NOM
+reader.ReadStartElement("Nom");
+///AVANCE EN LISANT SOUS FORME DE CHAINE
+personne.Nom = reader.ReadContentAsString();
+///AVANCE SUR LA BALISE DE FIN
+reader.ReadEndElement();
+
+///RECOMMENCE AVEC LA BALISE PRENOM
+reader.ReadStartElement("Prenom");
+personne.Prenom = reader.ReadContentAsString();
+reader.ReadEndElement();
+
+///RECOMMENCE AVEC LA BALISE DATEDENAISSANCE
+reader.ReadStartElement("DateDeNaissance");
+personne.DateDeNaissance = reader.ReadContentAsDateTime();
+reader.ReadEndElement();
+
+///RECOMMENCE AVEC LA TAILLE
+reader.ReadStartElement("DateDeNaissance");
+personne.Taille = reader.ReadContentAsInt();
+reader.ReadEndElement();
+
+///LIT LA BALISE DE FERMETURE DU DOCUMENT
+reader.ReadEndElement();
+
+System.Console.WriteLine($"{personne.Prenom} {personne.Nom} né le {personne.DateDeNaissance:dd/MM/yyyy} mesure {personne.Taille}cm");
+
+
